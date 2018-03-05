1643700bce8eca001b4ba63cab1900c743e7e072
Fritz <fre.healy@gmail.com>
2018-03-05 15:31:42 -0500

    completing about page

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -40,6 +40,45 @@
 	<?php endif;?>
 	<div class="row-3">
 		<div class="wrapper">
+			<?php $team_title = get_field("team_title");
+			$read_more_text = get_field("read_more_text");
+			if($team_title):?>
+				<header>
+					<h2><?php echo $team_title;?></h2>
+				</header>
+			<?php endif;
+			$args = array(
+				'post_per_page'=>-1,
+				'post_type'=>'team'
+			);
+			$query = new WP_Query($args);
+			if($query->have_posts()):?>
+				<div class="team clear-bottom">
+					<?php while($query->have_posts()): $query->the_post();
+						$image = get_field("image");
+						$email = get_field("email");?>
+						<div class="member js-blocks">
+							<?php if($image):?>
+								<img src="<?php echo $image['sizes']['large'];?>" alt="<?php echo $image['alt'];?>">
+							<?php endif;?>
+							<header>
+								<h3><?php the_title();?></h3>
+							</header>
+							<div class="spacer"></div>
+							<?php if($email):?>
+								<div class="email">
+									<?php echo $email;?>
+								</div><!--.email-->
+							<?php endif;
+							if($read_more_text):?>
+								<a class="button" href="<?php the_permalink();?>">
+									<?php echo $read_more_text;?>
+								</a>
+							<?php endif;?>
+						</div><!--.member-->
+					<?php endwhile;?>
+				</div><!--.team-->
+			<?php endif;?>
 		</div><!--.wrapper-->
 	</div><!--.row-3-->
 </article><!-- #post-## -->
