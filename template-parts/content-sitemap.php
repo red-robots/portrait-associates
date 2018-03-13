adbd35c125f21c33af6a3bb38ba26f0cf8a46f6b
Fritz <fre.healy@gmail.com>
2018-03-13 16:32:34 -0400

    formatting generic sitemap and 404 pages

--- template-parts/content-sitemap.php
+++ template-parts/content-sitemap.php
@@ -9,13 +9,14 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class("template-page"); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-sitemap"); ?>>
 	<div class="wrapper">
 		<header>
 			<h1><?php the_title();?></h1>
 		</header>
 		<section class="copy">
 			<?php the_content();?>
+			<?php wp_nav_menu( array( 'theme_location' => 'sitemap' ) ); ?>
 		</section><!--.copy-->
 	</div><!--.wrapper-->
 </article><!-- #post-## -->
