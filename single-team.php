7de00ba5acf7098aff07435d4d631d2f7559c4a0
Fritz <fre.healy@gmail.com>
2018-03-14 15:46:16 -0400

    adding email signup box

---
+++ single-team.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all single portfolio posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package ACStarter
+ */
+
+wp_redirect(get_permalink(2));
+exit(0);
+
+get_header(); ?>
+
+	<div id="primary" class="">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', 'single-artist' );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
