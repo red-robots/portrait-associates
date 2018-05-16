fab9972897b8bf9c699791eb129b9baf324a9c6e
Austin Crane <austincrane@gmail.com>
2018-05-16 13:00:08 -0400

    add tracking info

--- footer.php
+++ footer.php
@@ -57,6 +57,14 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119334887-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-119334887-1');
+</script>
 </body>
 </html>
